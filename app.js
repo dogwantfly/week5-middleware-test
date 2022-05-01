2c4e784f3848a9bf4e5f3120386f4a2a7bb3cf72
hsin-yu <tina2793778@gmail.com>
2022-05-02 01:52:37 +0800

    新增 appError.js

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use('/posts', postsRouter);
 
 // error handler
 app.use(function(err, req, res, next) {
+  console.log('err');
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
   res.status(err.statusCode).json({
